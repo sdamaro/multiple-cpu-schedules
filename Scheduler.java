13bf41fd3a9daf3c9565172dcc15a3a4c49d6642
grimertop90@gmail.com <grimertop90@gmail.com@5e4d2a62-6d35-873a-b1f3-62e1fe4b3c84>
2011-03-29 05:07:42 +0000

    Made checkNewProcess() abstract once again and added the override to RoundRobin.java.

--- Scheduler.java
+++ Scheduler.java
@@ -10,12 +10,7 @@ public abstract class Scheduler {
 	private Vector <Process> inactive;
 	
 	public abstract void Run();
-	public void checkNewProcesses() { 
-		while (inactive.get(0).startTime >= currentTime) {
-		   queue.add(inactive.get(0));
-		   System.out.println("[time " + currentTime + "ms] Process " + inactive.get(0).ID + " created (requiring " + inactive.get(0).neededCPUTime + "ms CPU time, priority " + inactive.get(0).priority + ")");
-		   inactive.remove(0);
-		}
+	public abstract void checkNewProcesses();
 	public abstract void addProcesses(Vector <Process> p) {
 		inactive = p;
 		Collections.sort(inactive, new startTimeComparator());
