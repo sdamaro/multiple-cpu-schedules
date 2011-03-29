901a16ebaf50c92d8c33320ae4c02c410eadcb51
grimertop90@gmail.com <grimertop90@gmail.com@5e4d2a62-6d35-873a-b1f3-62e1fe4b3c84>
2011-03-29 19:55:33 +0000

    Created Process::reset(). MainApp.java now works like a charm, as do FCFS and RR.

--- Process.java
+++ Process.java
@@ -10,14 +10,24 @@ public class Process {
 	public int turnaroundTime;
 	public int waitTime;
 	
+	private int initialNeeded;
+	
+	
 	public Process(int ID, int startTime) {
 		this.ID		     = ID;
 		this.startTime   = startTime;
 		neededCPUTime    = 500 + (int)(Math.round(Math.random() * 9500));
+		initialNeeded	 = neededCPUTime;
 		priority 	     = (int)(Math.round(Math.random() * 4));
 		turnaroundTime 	 = 0;
 		waitTime	   	 = 0;
 		lastTimeAccessed = 0;
 	}
 	
+	public void reset() {
+		neededCPUTime    = initialNeeded;
+		turnaroundTime   = 0;
+		waitTime	   	 = 0;
+		lastTimeAccessed = 0;
+	}
 }
