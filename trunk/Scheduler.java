1d43757501d1a24c667a0297accf1a64dd5b7dac
bethanyclark831@gmail.com <bethanyclark831@gmail.com@5e4d2a62-6d35-873a-b1f3-62e1fe4b3c84>
2011-03-29 06:38:00 +0000

    Hopefully near-final version of SJF. Also, wrote the printMMM function in Scheduler.
    PreemptivePriority.java NEEDS a lot of work!

--- trunk/Scheduler.java
+++ trunk/Scheduler.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.text.NumberFormat;
 
 public abstract class Scheduler {
 	boolean stillRunning = true;
@@ -16,5 +17,33 @@ public abstract class Scheduler {
 		inactive.addAdd(p);
 		Collections.sort(inactive, new startTimeComparator());
 	}
-
+	public abstract void contextSwitch();
+	
+	//this function finds the min, max, and ave values for the wait and turnaround times to correct precision
+	public void printMMMstats(){
+		float wtmin = 1000000, wtmax = 0, wttotal = 0;
+		float ttmin = 1000000, ttmax = 0, tttotal = 0;
+		for (int i = 0; i < waitTimes.size(); i++){
+			if(waitTimes.get(i) < wtmin) wtmin = waitTimes.get(i);
+			if(waitTimes.get(i) > wtmax) wtmax = waitTimes.get(i);
+			if(turnaroundTimes.get(i) < ttmin) ttmin = turnaroundTimes.get(i);
+			if(turnaroundTimes.get(i) > ttmax) ttmax = turnaroundTimes.get(i);
+			wttotal += waitTimes.get(i);
+			tttotal += turnaroundTimes.get(i);
+		}
+		wttotal = wttotal/waitTimes.size();
+		tttotal = tttotal/waitTimes.size();
+		
+		NumberFormat nf = NumberFormat.getInstance();
+		nf.setMaximumFractionDigits(3);
+		nf.setMinimumFractionDigits(3);
+		System.out.println("Wait Times:");
+		System.out.print("	 Minimum: " + nf.format(wtmin));
+		System.out.print("	 Maximum: " + nf.format(wtmax));
+		System.out.print("	 Average: " + nf.format(wttotal) + "\n");
+		System.out.println("Turnaround Times:");
+		System.out.print("	 Minimum: " + nf.format(ttmin));
+		System.out.print("	 Maximum: " + nf.format(ttmax));
+		System.out.print("	 Average: " + nf.format(tttotal) + "\n");
+	}
 }
