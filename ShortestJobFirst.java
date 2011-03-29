3f3f15cf306fb30e080cddeb5465855522002e4d
bethanyclark831@gmail.com <bethanyclark831@gmail.com@5e4d2a62-6d35-873a-b1f3-62e1fe4b3c84>
2011-03-29 04:57:01 +0000

    Added the rest of the Algorithm files.
    Made big changes to FirstComeFirstServe.java and Scheduler
    
    The concept of an inactive queue was added.

--- ShortestJobFirst.java
+++ ShortestJobFirst.java
@@ -3,25 +3,53 @@ public class ShortestJobFirst extends Scheduler {
 	int timeSlice = 100;
 	
 	@Override
-	public void Run() {
-		while (stillRunning) {
-			currentTime +=1;
-			if (queue.get(0).neededCPUTime == 0) {
+	public void Run(){
+		checkNewProcesses();
+		while(!queue.isEmpty() || !inactive.isEmpty()){
+			if (!queue.isEmpty()){
+				if (queue.get(0).startedProcessing == false){
+					System.out.println("[time " + currentTime + "ms] Process " + queue.get(0).ID + " accessed CPU for the first time (wait time  " + queue.get(0).waitTime + "ms)");
+				}
+				//new process is entering the system. declaring the wait time and outputting to screen.
+				queue.get(0).waitTime != (currentTime - queue.get(0).lastTimeAccessed);
+				
+				
+				//incrementing each millisecond at a time for the breadth of the current process
+				while (queue.get(0).neededCPUTime > 0){
+					queue.get(0).neededCPUTime--;
+					currentTime++;
+					checkNewProcesses();
+				}
+				
+				//finishing up the process by defining the turnaround times and output
+				queue.get(0).turnaroundTime = currentTime - queue.get(0).waitTime;
+				turnaroundTimes.add(queue.get(0).turnaroundTime);
+				System.out.println("[time " + currentTime + "ms] Process " + queue.get(0).ID + " terminated (turnaround time " + queue.get(0).turnaroundTime + "ms, wait time " + queue.get(0).waitTime + "ms)");
+				
+				//keeping track of context switches for all except the last transition.
+				if(!(queue.get(0).ID == sizeOfQueue)){
+					contextSwitch();
+				}
 				queue.remove(0);
-				continue;
 			}
-			if (currentTime % timeSlice == 0)
-				contextSwitch();
+			else{
+				currentTime++;
+				checkNewProcesses();
+			}
 		}
 	}
 	
 	@Override
-	public void addProcesses() {
-		queue.add(p);
+	public void contextSwitch(){
+		System.out.println("[time " + currentTime + "ms] Context switch (swapped out process) " + queue.get(0).ID + " for process " + queue.get(1).ID + ")");
+		for (int j = 0; j < 9; j++){
+			currentTime++;
+			checkNewProcesses();
+		}
 	}
-	
+
 	@Override
-	public void contextSwitch() {
+	public void addProcesses(){
 		
 	}
 }
