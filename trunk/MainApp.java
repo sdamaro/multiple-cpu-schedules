3a01716f2b80639321aab90cf1d859d1e88f998d
grimertop90@gmail.com <grimertop90@gmail.com@5e4d2a62-6d35-873a-b1f3-62e1fe4b3c84>
2011-03-29 19:55:33 +0000

    Created Process::reset(). MainApp.java now works like a charm, as do FCFS and RR.

--- trunk/MainApp.java
+++ trunk/MainApp.java
@@ -4,29 +4,26 @@ public class MainApp {
 	
 	@SuppressWarnings("unchecked")
 	public static void main(String [] args) {
-		Vector <Process> queue1 = new Vector <Process>();
+		Vector <Process> queue = new Vector <Process>();
 		int numProcesses = 20;
 		
 		for (int i=1; i<=numProcesses; i++) {
-			queue1.add(new Process(i, 0));
+			queue.add(new Process(i, 0));
 		}
 		
-		// Make 3 copies of the queue, since the queue is altered permanently by each Scheduler
-		Vector <Process> queue2 = (Vector <Process>)queue1.clone();
-		//Vector <Process> queue3 = (Vector <Process>)queue1.clone();
-		//Vector <Process> queue4 = (Vector <Process>)queue1.clone();
-		
-		Simulate(new RoundRobin(), queue2);
-		Simulate(new FirstComeFirstServe(), queue1);
-		
-		//Simulate(new ShortestJobFirst(), queue3);
-		//Simulate(new PreemptivePriority(), queue4);
+		Simulate(new FirstComeFirstServe(), queue);
+		Simulate(new RoundRobin(), queue);
+		//Simulate(new ShortestJobFirst(), queue);
+		//Simulate(new PreemptivePriority(), queue);
 		
 	}
 	
 	static void Simulate(Scheduler s, Vector <Process> v) {
 		s.addProcesses(v);
 		s.Run();
+		for (Process p : v) {
+			p.reset();
+		}
 	}
 	
 }
