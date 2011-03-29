08b8008160bc46d1f9f70e76b4f5a3366533021e
grimertop90@gmail.com <grimertop90@gmail.com@5e4d2a62-6d35-873a-b1f3-62e1fe4b3c84>
2011-03-29 05:55:11 +0000

    Cleaned up the Process.java and Scheduler.java files a little.

--- Process.java
+++ Process.java
@@ -9,12 +9,13 @@ public class Process {
 	public int waitTime;
 	
 	public Process(int ID, int startTime) {
-		this.ID		   = ID;
-		this.startTime = startTime;
-		neededCPUTime  = 500 + (int)(Math.round(Math.random() * 9500));
-		priority 	   = (int)(Math.round(Math.random() * 4));
-		turnaroundTime = 0;
-		waitTime	   = 0;
+		this.ID		     = ID;
+		this.startTime   = startTime;
+		neededCPUTime    = 500 + (int)(Math.round(Math.random() * 9500));
+		priority 	     = (int)(Math.round(Math.random() * 4));
+		turnaroundTime 	 = 0;
+		waitTime	   	 = 0;
+		lastTimeAccessed = 0;
 	}
 	
 	
