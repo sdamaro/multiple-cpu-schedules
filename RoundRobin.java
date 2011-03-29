23a42790e729b3ecb569ca4577a0578825b2811a
grimertop90@gmail.com <grimertop90@gmail.com@5e4d2a62-6d35-873a-b1f3-62e1fe4b3c84>
2011-03-29 03:32:31 +0000

    Added some stuff to the RoundRobin.java file: - Run() has been expanded to take a time slice - contextSwitch() now has an empty method

--- RoundRobin.java
+++ RoundRobin.java
@@ -1,12 +1,26 @@
 public class RoundRobin extends Scheduler {
+	int timeSlice = 100;
 	
 	@Override
 	public void Run() {
-		
+		while (stillRunning) {
+			currentTime +=1;
+			if (queue.get(0).neededCPUTime == 0) {
+				queue.remove(0);
+				continue;
+			}
+			if (currentTime % timeSlice == 0)
+				contextSwitch();
+		}
 	}
 	
 	@Override
 	public void addProcesses() {
-		queue.
+		queue.add(p);
+	}
+	
+	@Override
+	public void contextSwitch() {
+		
 	}
 }
