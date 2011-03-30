735110d5ae16818677bbcede9a1483b81b05a7d1
grimertop90@gmail.com <grimertop90@gmail.com@5e4d2a62-6d35-873a-b1f3-62e1fe4b3c84>
2011-03-30 00:39:13 +0000

    I THINK everything is working for both parts. Server testing next.

--- trunk/ShortestJobFirst.java
+++ trunk/ShortestJobFirst.java
@@ -58,7 +58,7 @@ public class ShortestJobFirst extends Scheduler {
 
 	@Override
 	void checkNewProcesses(){ 
-		while (!inactive.isEmpty() && inactive.get(0).startTime >= currentTime) {
+		while (!inactive.isEmpty() && inactive.get(0).startTime <= currentTime) {
 		   queue.add(inactive.get(0));
 		   System.out.println("[time " + currentTime + "ms] Process " + inactive.get(0).ID + " created (requiring " + inactive.get(0).neededCPUTime + "ms CPU time, priority " + inactive.get(0).priority + ")");
 		   inactive.remove(0);
